a7ea536603cc03b5d297bafca299cb0113fed606
Thiago Xavier <thiagoxavier9966@gmail.com>
2022-04-03 13:52:28 -0300

    Footer parcialmente feito

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+import Youtube from './img/YouTube-Icon-Full-Color-Logo.wine.svg'
+import Instagran from './img/Instagram-Logo.wine.svg'
+import Twitter from './img/Twitter-Logo.wine.svg'
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h1>Todos direitos reservados</h1>
+    </div>
+  );
+}
+
+export default Footer;
